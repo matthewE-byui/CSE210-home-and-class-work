18b6924adf8e3595cad91e342130d61df6cf0ea8
Matthew Edgar <edgarmatt26@byui.edu>
2025-12-01 19:29:39 -0700

    .sln is causing mayjor issues.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
-// using System;
+using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Hello Learning05 World!");
-//     }
-// }
+namespace employee_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HourlyEmployee hEmployee = new HourlyEmployee();
+            hEmployee.SetName("John Doe");
+            hEmployee.SetIdNumber("123abc");
+            hEmployee.SetPayRate(15);
+            hEmployee.SetHoursWorked(35);
+
+            SalaryEmployee sEmployee = new SalaryEmployee();
+            sEmployee.SetName("Jane Smith");
+            sEmployee.SetIdNumber("456def");
+            sEmployee.SetSalary(60000);
+
+
+            DisplayEmployeeInformation(hEmployee);
+            DisplayEmployeeInformation(sEmployee);
+        }
+        public static void DisplayEmployeeInformation(employee_demo employee)
+        {
+            Console.WriteLine($"{employee.GetName()}");
+            Console.WriteLine("Employee ID: " + employee.GetIdNumber());
+        }
+    }
+}
